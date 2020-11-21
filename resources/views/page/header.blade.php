c614c9e8bd1e1834fb38c2c3d39b45fd445e13db
= <=>
2020-11-21 20:48:45 +0700

    setup menu bar all page

--- resources/views/page/header.blade.php
+++ resources/views/page/header.blade.php
@@ -90,16 +90,16 @@
              <div class="visible-xs clearfix"></div>
              <nav class="main-menu">
                  <ul class="l-inline ov">
-                     <li><a href="index.html">Trang chủ</a></li>
-                     <li><a href="#">Sản phẩm</a>
+                     <li><a href="{{URL::route('trangchu')}}">Trang chủ</a></li>
+                     <li><a href="">Sản phẩm</a>
                          <ul class="sub-menu">
-                             <li><a href="product_type.html">Sản phẩm 1</a></li>
-                             <li><a href="product_type.html">Sản phẩm 2</a></li>
-                             <li><a href="product_type.html">Sản phẩm 4</a></li>
+                             @foreach($loai_sp as $loai)
+                             <li><a href="{{URL::route('loaisanpham')}}">{{$loai->name}}</a></li>
+                             @endforeach
                          </ul>
                      </li>
-                     <li><a href="about.html">Giới thiệu</a></li>
-                     <li><a href="contacts.html">Liên hệ</a></li>
+                     <li><a href="{{URL::route('gioithieu')}}">Giới thiệu</a></li>
+                     <li><a href="{{URL::route('lienhe')}}">Liên hệ</a></li>
                  </ul>
                  <div class="clearfix"></div>
              </nav>
