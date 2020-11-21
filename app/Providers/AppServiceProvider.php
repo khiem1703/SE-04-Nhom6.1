b3fe97655d08fa4bac2aa71722a3e5b1031d3d05
= <=>
2020-11-21 20:53:19 +0700

    setup link and submenu from database

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\ProductType ;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        $loai_sp = ProductType::all();
+        View::share('loai_sp', $loai_sp);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('index',[
-	'as'=>'trang-chu',
+	'as'=>'trangchu',
 	'uses' => 'PageController@getIndex'
 ]);
 Route::get('loai-san-pham',[
