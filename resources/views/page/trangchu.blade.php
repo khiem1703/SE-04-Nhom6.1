b44a5cacf60e51786556747dbbbb3b339b2537e8
= <=>
2020-11-24 15:13:31 +0700

    creat gioHang and addToCart in index

--- resources/views/page/trangchu.blade.php
+++ resources/views/page/trangchu.blade.php
@@ -64,7 +64,9 @@
 										</p>
 									</div>
 									<div class="single-item-caption">
-										<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+										<a class="add-to-cart pull-left" 
+										href="{{route('themgiohang',$new->id)}}">
+											<i class="fa fa-shopping-cart"></i></a>
 										<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
 										<div class="clearfix"></div>
 									</div>
