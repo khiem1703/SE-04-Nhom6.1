7c3e1e4cbae86930504abaef3fe7027b89257d05
NgoThien963 <ngthien.130699@gmail.com>
2020-12-16 16:11:15 +0700

    tao trang Lien He

--- resources/views/page/lienhe.blade.php
+++ resources/views/page/lienhe.blade.php
@@ -23,51 +23,50 @@
             <div class="space50">&nbsp;</div>
             <div class="row">
                 <div class="col-sm-8">
-                    <h2>Contact Form</h2>
+                    <h2>Mẫu Liên Hệ</h2>
                     <div class="space20">&nbsp;</div>
-                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit ani m id est laborum.</p>
+                    <p>Quý khách có thể liên hệ với chúng tôi từ website này bằng cách điền đầy đủ thông tin, chúng tôi sẽ liên lạc với Quý khách sớm nhất!</p>
                     <div class="space20">&nbsp;</div>
                     <form action="#" method="post" class="contact-form">
                         <div class="form-block">
-                            <input name="your-name" type="text" placeholder="Your Name (required)">
+                            <input name="your-name" type="text" placeholder="Họ và tên (bắt buộc)">
                         </div>
                         <div class="form-block">
-                            <input name="your-email" type="email" placeholder="Your Email (required)">
+                            <input name="your-email" type="email" placeholder="Email (bắt buộc)">
                         </div>
                         <div class="form-block">
-                            <input name="your-subject" type="text" placeholder="Subject">
+                            <input name="your-subject" type="text" placeholder="Số điện thoại (bắt buộc)">
                         </div>
                         <div class="form-block">
-                            <textarea name="your-message" placeholder="Your Message"></textarea>
+                            <input name="your-subject" type="text" placeholder="Địa chỉ (bắt buộc)">
                         </div>
                         <div class="form-block">
-                            <button type="submit" class="beta-btn primary">Send Message <i class="fa fa-chevron-right"></i></button>
+                            <textarea name="your-message" placeholder="Nội dung"></textarea>
+                        </div>
+                        <div class="form-block">
+                            <button type="submit" class="beta-btn primary">Gửi <i class="fa fa-chevron-right"></i></button>
                         </div>
                     </form>
                 </div>
                 <div class="col-sm-4">
-                    <h2>Contact Information</h2>
+                    <h2>Baker's Alley Cake</h2>
                     <div class="space20">&nbsp;</div>
 
-                    <h6 class="contact-title">Address</h6>
+                    <h6 class="contact-title">Địa chỉ</h6>
                     <p>
-                        Suite 127 / 267 – 277 Brussel St,<br>
-                        62 Croydon, NYC <br>
-                        Newyork
+                        số 334 Nguyễn Trãi,<br>
+                        Quận Thanh Xuân, <br>
+                        Thành phố Hà Nội.
                     </p>
                     <div class="space20">&nbsp;</div>
-                    <h6 class="contact-title">Business Enquiries</h6>
+                    <h6 class="contact-title">Điện thoại</h6>
                     <p>
-                        Doloremque laudantium, totam rem aperiam, <br>
-                        inventore veritatio beatae. <br>
-                        <a href="mailto:biz@betadesign.com">biz@betadesign.com</a>
+                        0123456789 <br>
                     </p>
                     <div class="space20">&nbsp;</div>
-                    <h6 class="contact-title">Employment</h6>
+                    <h6 class="contact-title">Email</h6>
                     <p>
-                        We’re always looking for talented persons to <br>
-                        join our team. <br>
-                        <a href="hr@betadesign.com">hr@betadesign.com</a>
+                        <a href="hr@betadesign.com">BACake@gmail.com</a>
                     </p>
                 </div>
             </div>
