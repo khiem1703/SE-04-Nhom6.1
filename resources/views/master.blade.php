a9e97e59c891744a5c3b3edbfa62520685569175
unknown <dongoc4799@gmail.com>
2020-12-23 17:54:25 +0700

    add  messenge

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -20,13 +20,13 @@
 
 <body>
 
-   @include('header')
+    @include('header')
     <div class="rev-slider">
-        @yield('content')  
+        @yield('content')
     </div> <!-- .container -->
 
     @include('footer')
-    
+
 
 
     <!-- include js files -->
@@ -42,6 +42,30 @@
     <script src="source/assets/dest/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
     <script src="source/assets/dest/js/waypoints.min.js"></script>
     <script src="source/assets/dest/js/wow.min.js"></script>
+    <!-- fb -->
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                xfbml: true,
+                version: 'v9.0'
+            });
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <!-- Your Chat Plugin code -->
+    <div  class="fb-customerchat" attribution=setup_tool page_id="110772597472841" theme_color="#13cf13" logged_in_greeting="Hi! How can we help you?" logged_out_greeting="Hi! How can we help you?">
+    </div>
     <!--customjs-->
     <script src="source/assets/dest/js/custom2.js"></script>
     <script>
